c2a98cb1707368db28a51840d1cff12c5770e548
hongduck710 <hongik710@gmail.com>
2024-09-17 21:10:12 +0900

    #3.4 Log In 4:29까지 진도 나감

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArNn33Zvi9qWwVKleg_0-mIEH4RJX1OVs",
+  authDomain: "nwitter-reloaded-19a9e.firebaseapp.com",
+  projectId: "nwitter-reloaded-19a9e",
+  storageBucket: "nwitter-reloaded-19a9e.appspot.com",
+  messagingSenderId: "316430622602",
+  appId: "1:316430622602:web:b3af2d429e8d03ae25b273"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
