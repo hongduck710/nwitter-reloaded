cf767eb45d71e7f42cbaa7f8e3076303a9cfe4e3
hongduck710 <hongik710@gmail.com>
2024-09-18 20:01:57 +0900

    220240918 #4.2 Tweeting to Firestore 2:57까지 진도 나감.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyArNn33Zvi9qWwVKleg_0-mIEH4RJX1OVs",
@@ -13,5 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
